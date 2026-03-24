c6b922da19e1316a8710824249c4892fb465d6a2
KBLSolutions <avoloshin@kbl-solutions.com>
2026-03-24 11:23:35 +0200

    Update index.mjs

--- commands/help/index.mjs
+++ commands/help/index.mjs
@@ -1,6 +1,6 @@
 const output = [
 	"Available commands:",
-	"briefing, squad, planet,",
+	"briefing, squad, status, planet,",
 	"snake, pong, fire,",
 	"help, screensaver, clear, logout, reboot, quit"
 ];
